1d0fa73b6b15e53cc3e948643d28db30ba12d009
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-11-21 22:48:50 -0300

    Add id property to ShopItemsData object

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 let shop = document.getElementById('shop');
 
 let shoopItemsData = [{
-    id: Date.now(),
+    id: Math.ceil(Math.random()*1000000),
     name: "Casual Shirt",
     price: 45,
     desc: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
     img: "images/img-1.jpg"
     },
     {
-        id: Date.now(),
+        id: Math.ceil(Math.random()*1000000),
         name: "Office Shirt",
         price: 55,
         desc: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain",
         img: "images/img-2.jpg"
     },
     {
-        id: Date.now(),
+        id: Math.ceil(Math.random()*1000000),
         name: "T Shirt",
         price: 35,
         desc: "Sed ante lacus, posuere sit amet justo sed, ultrices imperdiet nulla. Quisque aliquam rhoncus elit, in congue velit elementum et",
         img: "images/img-3.jpg"
     },
     {
-        id: Date.now(),
+        id: Math.ceil(Math.random()*1000000),
         name: "Mens Suit",
         price: 450,
         desc: "Nunc in lectus sollicitudin, porttitor augue consectetur, aliquam eros. Nullam et pharetra ex, id pretium nisl",
@@ -32,10 +32,10 @@ let shoopItemsData = [{
 
 let generateShop = () => {
     return (shop.innerHTML = shoopItemsData.map((product) =>{
-        const {name, price, desc, img} = product;
+        const {id, name, price, desc, img} = product;
         
         return  `
-        <div class="item">
+        <div id="product-id-${id} "class="item">
             <img width="220" src="${img}" alt="">
             <div class="details">
                 <h3>${name}</h3>
@@ -44,7 +44,7 @@ let generateShop = () => {
                     <h2>$ ${price}</h2>
                     <div class="buttons">
                         <i class="bi bi-dash-lg"></i>
-                        <div class="quantity">0</div>
+                        <div id=${id} class="quantity">0</div>
                         <i class="bi bi-plus-lg"></i>
                     </div>
                 </div>
