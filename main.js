f1abd7c3a4f7106da51293cc4b83e5a31a10c485
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-12-02 22:28:23 -0300

    Add decrement function

--- main.js
+++ main.js
@@ -79,7 +79,18 @@ let increment = (id) => {
 }
 
 let decrement = (id) => {
-    console.log('Decrement', id);
+    let selectedItem = id;
+    // console.log(id);
+    let search = basket.find((x)=> x.id === selectedItem.id);
+    // console.log(search);
+    if(search.item === 0){
+        return;
+    } else {
+        search.item -= 1;
+    }
+
+    
+    console.log(basket);
 }
 
 let update = () => {
